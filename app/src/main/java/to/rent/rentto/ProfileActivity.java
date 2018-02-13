002b9a733bbc1f9c711e82ab285814e28063a6ef
Brandon Chen <Brandon@Brandon.(none)>
2018-02-12 16:54:10 -0800

    complete tabs setup

--- app/src/main/java/to/rent/rentto/ProfileActivity.java
+++ app/src/main/java/to/rent/rentto/ProfileActivity.java
@@ -1,8 +1,9 @@
 package to.rent.rentto;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,21 +12,29 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import to.rent.rentto.Utils.BottomNavigationViewHelper;
 
-public class HomeActivity extends AppCompatActivity {
+/**
+ * Created by Brandon on 2/12/2018.
+ */
 
-    private static final String TAG = "HomeActivity";
+public class ProfileActivity extends AppCompatActivity {
+    private static final String TAG = "ProfileActivity";
 
-    private Context mContext = HomeActivity.this;
-    private static final int ACTIVITY_NUM = 0;
+    private Context mContext = ProfileActivity.this;
+    private static final int ACTIVITY_NUM = 2;
+
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Log.d(TAG, "onCreate: Starting. ");
+        Log.d(TAG, "onCreate: Started.");
+
         setupBottomNavigationView();
+
+
     }
+
 
     private void setupBottomNavigationView(){
         Log.d(TAG, "setupBottomNavigationView: setting up bottomnavigationview");
@@ -36,5 +45,4 @@ public class HomeActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
-
 }
--- app/src/main/java/to/rent/rentto/Utils/BottomNavigationViewHelper.java
+++ app/src/main/java/to/rent/rentto/Utils/BottomNavigationViewHelper.java
@@ -1,8 +1,22 @@
 package to.rent.rentto.Utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Camera;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.net.Inet4Address;
+
+import to.rent.rentto.CameraActivity;
+import to.rent.rentto.HomeActivity;
+import to.rent.rentto.ProfileActivity;
+import to.rent.rentto.R;
+import to.rent.rentto.SearchActivity;
 
 /**
  * Created by iguest on 2/11/18.
@@ -17,5 +31,32 @@ public class BottomNavigationViewHelper {
         bottomNavigationViewEx.enableItemShiftingMode(false);
         bottomNavigationViewEx.enableShiftingMode(false);
         bottomNavigationViewEx.setTextVisibility(false);
+    }
+
+    public static void enableNavigation(final Context context, BottomNavigationViewEx view) {
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.ic_house:
+                        Intent intent1 = new Intent(context, HomeActivity.class); //ACTIVITY_NUM = 0
+                        context.startActivity(intent1);
+                        break;
+
+                    case R.id.ic_search:
+                        Intent intent2 = new Intent(context, CameraActivity.class); //ACTIVITY_NUM = 1
+                        context.startActivity(intent2);
+                        break;
+
+                    case R.id.ic_circle:
+                        Intent intent3 = new Intent(context, ProfileActivity.class); //ACTIVITY_NUM = 2
+                        context.startActivity(intent3);
+                        break;
+                }
+
+
+                return false;
+            }
+        });
     }
 }
