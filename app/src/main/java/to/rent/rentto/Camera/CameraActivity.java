70dff9cca9cdedaa6b67b4665122571d57d5f022
Brandon Chen <Brandon@Brandon.(none)>
2018-02-12 21:45:21 -0800

    connect camera activity

--- app/src/main/java/to/rent/rentto/Camera/CameraActivity.java
+++ app/src/main/java/to/rent/rentto/Camera/CameraActivity.java
@@ -26,7 +26,7 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_camera);
         Log.d(TAG, "onCreate: Started.");
 
         setupBottomNavigationView();
