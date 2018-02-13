ad4b09bd7926aa7b4bd20713bff6aea64b4be024
Brandon Chen <Brandon@Brandon.(none)>
2018-02-12 17:16:34 -0800

    add fragment templates

---
+++ app/src/main/java/to/rent/rentto/Home/MessagesFragment.java
@@ -0,0 +1,27 @@
+package to.rent.rentto.Home;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import to.rent.rentto.R;
+
+/**
+ * Created by Brandon on 2/12/2018.
+ */
+
+public class MessagesFragment extends Fragment {
+    public static final String TAG = "MessagesFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_messages, container, false);
+
+        return view;
+        
+    }
+}
