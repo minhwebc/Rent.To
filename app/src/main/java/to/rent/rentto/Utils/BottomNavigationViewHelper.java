15d2be4159581c9db0e501159fbecc17e4c0ad14
Allen Cho <allencho@uw.edu>
2018-02-14 09:07:30 -0800

    Activity Navigation for Bottom NavigationView

--- app/src/main/java/to/rent/rentto/Utils/BottomNavigationViewHelper.java
+++ app/src/main/java/to/rent/rentto/Utils/BottomNavigationViewHelper.java
@@ -1,8 +1,16 @@
 package to.rent.rentto.Utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import to.rent.rentto.HomeActivity;
+import to.rent.rentto.R;
 
 /**
  * Created by iguest on 2/11/18.
@@ -17,5 +25,28 @@ public class BottomNavigationViewHelper {
         bottomNavigationViewEx.enableItemShiftingMode(false);
         bottomNavigationViewEx.enableShiftingMode(false);
         bottomNavigationViewEx.setTextVisibility(false);
+    }
+
+    public static void enableNavigation(final Context context,BottomNavigationViewEx view) {
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.ic_home:
+                        Intent intent1 = new Intent(context, HomeActivity.class);
+                        context.startActivity(intent1);
+                        break;
+                    case R.id.ic_addImage:
+                        Intent intent2 = new Intent(context, ShareActivity.class);
+                        context.startActivity(intent2);
+                        break;
+                    case R.id.ic_profile:
+                        Intent intent3 = new Intent(context, ProfileActivity.class);
+                        context.startActivity(intent3);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/to/rent/rentto/Utils/ShareActivity.java
+++ app/src/main/java/to/rent/rentto/Utils/ShareActivity.java
@@ -1,29 +1,33 @@
-package to.rent.rentto;
+package to.rent.rentto.Utils;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-import to.rent.rentto.Utils.BottomNavigationViewHelper;
-import to.rent.rentto.Utils.ProfileActivity;
+import to.rent.rentto.HomeActivity;
+import to.rent.rentto.R;
 
-public class  HomeActivity extends AppCompatActivity {
+/**
+ * Created by allencho on 2/14/18.
+ */
 
-    private static final String TAG = "HomeActivity";
+public class ShareActivity extends AppCompatActivity {
+    private static final String TAG = "ShareActivity";
 
-    private Context mContext = HomeActivity.this;
-    private static final int ACTIVITY_NUM = 0;
+    private Context mContext = ShareActivity.this;
+    private static final int ACTIVITY_NUM = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Log.d(TAG, "onCreate: Starting. ");
+        Log.d(TAG, "onCreate: starting");
 
         setupBottomNavigationView();
     }
@@ -40,5 +44,4 @@ public class  HomeActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
-
 }
