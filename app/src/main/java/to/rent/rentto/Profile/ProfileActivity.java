41de67ffde7e6275f0b1f25ee5162cdb84ad2c35
Allen Cho <allencho@uw.edu>
2018-02-15 00:27:14 -0800

    Profile Toolbar and Menu

--- app/src/main/java/to/rent/rentto/Profile/ProfileActivity.java
+++ app/src/main/java/to/rent/rentto/Profile/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,12 +27,30 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: starting");
 
-        setupBottomNavigationView();
+        // setupBottomNavigationView();
+        setupToolbar();
     }
 
+    private void setupToolbar() {
+        Toolbar toolbar =(Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: clicked menu item: " + item);
+
+                switch(item.getItemId()) {
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: Navigating to Profile Preferences.");
+                }
+                return false;
+            }
+        });
+    }
     /**
      * BottomNavigationView setup
      */
@@ -43,5 +62,11 @@ public class ProfileActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu,  menu);
+        return true;
     }
 }
