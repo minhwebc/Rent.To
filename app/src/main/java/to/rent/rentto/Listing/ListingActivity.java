728013293b2f0dc01423f0df38b035bc8b37e539
Sam Zeng <arosmas@gmail.com>
2018-02-15 08:55:21 -0800

    Updated toolbar for listing

--- app/src/main/java/to/rent/rentto/Listing/ListingActivity.java
+++ app/src/main/java/to/rent/rentto/Listing/ListingActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ListView;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
@@ -15,9 +14,6 @@ import java.util.ArrayList;
 
 import to.rent.rentto.R;
 import to.rent.rentto.Utils.BottomNavigationViewHelper;
-
-import static to.rent.rentto.Utils.BottomNavigationViewHelper.setupBottomNavigationView;
-
 
 /**
  * Created by Sora on 2/14/2018.
@@ -34,8 +30,10 @@ public class ListingActivity extends AppCompatActivity {
         mContext = ListingActivity.this;
         Log.d(TAG, "onCreate: Started.");
 
-        /*
-        ImageView backarrow = (ImageView) findViewByID(R.id.backArrow);
+
+        setupBottomNavigationView();
+
+        ImageView backarrow = (ImageView) findViewById(R.id.backArrow);
         backarrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,9 +41,6 @@ public class ListingActivity extends AppCompatActivity {
                 finish();
             }
         });
-        */
-
-        setupBottomNavigationView();
     }
 
     private void setupListing() {
