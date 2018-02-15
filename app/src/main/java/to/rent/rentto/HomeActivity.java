88b058f3f71f0ca8bfe7d8678eaf0d282e1265f7
Quan <minhquan611996@gmail.com>
2018-02-14 17:19:58 -0800

    done with login and registering

--- app/src/main/java/to/rent/rentto/HomeActivity.java
+++ app/src/main/java/to/rent/rentto/HomeActivity.java
@@ -1,16 +1,38 @@
 package to.rent.rentto;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
+import to.rent.rentto.Login.LoginActivity;
 import to.rent.rentto.Utils.BottomNavigationViewHelper;
 
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            Log.d(TAG, "user sign in");
+        } else {
+            Log.d(TAG, "user not sign in");
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
+        //updateUI(currentUser);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
