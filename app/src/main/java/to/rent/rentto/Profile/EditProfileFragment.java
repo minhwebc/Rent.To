ee132ffa85210d5ffd6facdc1a9064e2e03d8af2
Allen Cho <allencho@uw.edu>
2018-02-27 05:44:22 -0800

    Firebase User SignOut

--- app/src/main/java/to/rent/rentto/Profile/EditProfileFragment.java
+++ app/src/main/java/to/rent/rentto/Profile/EditProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,9 +18,11 @@ import to.rent.rentto.R;
 public class EditProfileFragment extends Fragment {
     private static final String TAG = "EditProfileFragment";
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.v(TAG, "here");
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
 
         return view;
