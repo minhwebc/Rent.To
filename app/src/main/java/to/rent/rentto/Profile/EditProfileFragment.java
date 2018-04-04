9398b636582384a34c01b97df64beb0035cb4df7
Allen Cho <allencho@uw.edu>
2018-04-04 05:42:25 -0700

    edit email (pt3/3)  done

--- app/src/main/java/to/rent/rentto/Profile/EditProfileFragment.java
+++ app/src/main/java/to/rent/rentto/Profile/EditProfileFragment.java
@@ -14,8 +14,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.ProviderQueryResult;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -35,8 +40,72 @@ import to.rent.rentto.Utils.FirebaseMethods;
  * Created by allencho on 2/15/18.
  */
 
-public class EditProfileFragment extends Fragment {
+public class EditProfileFragment extends Fragment implements
+    ConfirmPasswordDialog.OnConfirmPasswordListener{
     private static final String TAG = "EditProfileFragment";
+
+    @Override
+    public void onConfirmPassword(String password) {
+        Log.d(TAG, "onConfirmPassword: got the password: " + password);
+
+        // Get auth credentials from the user for re-authentication. The example below shows
+        // email and password credentials but there are multiple possible providers,
+        // such as GoogleAuthProvider or FacebookAuthProvider.
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(mAuth.getCurrentUser().getEmail(), password);
+
+        ///////////////////// Prompt the user to re-provide their sign-in credentials
+        mAuth.getCurrentUser().reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "User re-authenticated.");
+
+                            ///////////////////////check to see if the email is not already present in the database
+                            mAuth.fetchProvidersForEmail(mEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<ProviderQueryResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<ProviderQueryResult> task) {
+                                    if(task.isSuccessful()){
+                                        try{
+                                            if(task.getResult().getProviders().size() == 1){
+                                                Log.d(TAG, "onComplete: that email is already in use.");
+                                                Toast.makeText(getActivity(), "That email is already in use", Toast.LENGTH_SHORT).show();
+                                            }
+                                            else{
+                                                Log.d(TAG, "onComplete: That email is available.");
+
+                                                //////////////////////the email is available so update it
+                                                mAuth.getCurrentUser().updateEmail(mEmail.getText().toString())
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    Log.d(TAG, "User email address updated.");
+                                                                    Toast.makeText(getActivity(), "email updated", Toast.LENGTH_SHORT).show();
+                                                                    mFirebaseMethods.updateEmail(mEmail.getText().toString());
+                                                                }
+                                                            }
+                                                        });
+                                            }
+                                        }catch (NullPointerException e){
+                                            Log.e(TAG, "onComplete: NullPointerException: "  +e.getMessage() );
+                                        }
+                                    }
+                                }
+                            });
+
+
+
+
+
+                        }else{
+                            Log.d(TAG, "onComplete: re-authentication failed.");
+                        }
+
+                    }
+                });
+    }
 
     //firebase
     private FirebaseAuth mAuth;
@@ -129,7 +198,7 @@ public class EditProfileFragment extends Fragment {
             //          -Confirm the password and email
             ConfirmPasswordDialog dialog = new ConfirmPasswordDialog();
             dialog.show(getFragmentManager(), getString(R.string.confirm_password_dialog));
-
+            dialog.setTargetFragment(EditProfileFragment.this, 1);
 
             // step2) check if the email already is registered
             //          -'fetchProvidersForEmail(String email)'
@@ -256,4 +325,6 @@ public class EditProfileFragment extends Fragment {
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+
+
 }
--- app/src/main/java/to/rent/rentto/Utils/FirebaseMethods.java
+++ app/src/main/java/to/rent/rentto/Utils/FirebaseMethods.java
@@ -49,6 +49,10 @@ public class FirebaseMethods {
         }
     }
 
+    /**
+     * update username in the 'users' node and 'user_account_settings' node
+     * @param username
+     */
     public void updateUsername(String username) {
         Log.d(TAG, "updateUsername: updating username to: " + username);
         myRef.child(mContext.getString(R.string.dbname_users))
@@ -61,6 +65,21 @@ public class FirebaseMethods {
                 .child(mContext.getString(R.string.field_username))
                 .setValue(username);
     }
+
+    /**
+     * update the email in the 'user's' node
+     * @param email
+     */
+    public void updateEmail(String email){
+        Log.d(TAG, "updateEmail: upadting email to: " + email);
+
+        myRef.child(mContext.getString(R.string.dbname_users))
+                .child(userID)
+                .child(mContext.getString(R.string.field_email))
+                .setValue(email);
+
+    }
+
     public void sendVerificationEmail(){
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
