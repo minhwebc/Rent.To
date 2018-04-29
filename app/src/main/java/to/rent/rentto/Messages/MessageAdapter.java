9d93ce71448bf8d48fd59ec80f5ecc54143d27f7
Brandon Chen <chenb777@uw.edu>
2018-04-28 19:58:50 -0700

    can preview other profile pic in chatactivity

--- app/src/main/java/to/rent/rentto/Messages/MessageAdapter.java
+++ app/src/main/java/to/rent/rentto/Messages/MessageAdapter.java
@@ -2,6 +2,7 @@ package to.rent.rentto.Messages;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.util.Log;
@@ -9,7 +10,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -19,19 +30,42 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import to.rent.rentto.Models.Item;
 import to.rent.rentto.Models.Message;
+import to.rent.rentto.Models.UserAccountSettings;
+import to.rent.rentto.Models.UserSettings;
+import to.rent.rentto.Profile.ProfilePreviewActivity;
+import to.rent.rentto.Profile.ProfilePreviewFragment;
 import to.rent.rentto.R;
+import to.rent.rentto.Utils.FirebaseMethods;
+
+import static android.content.ContentValues.TAG;
 
 public class MessageAdapter extends BaseAdapter {
 
     public List<Message> messages = new ArrayList<Message>();
     Context context;
+    String otherProfilePicURL;
+    private String otherProfileUID;
+    private String currentUID;
+    private DatabaseReference mRef;
+    private FirebaseMethods mFirebaseMethods;
+    private ImageView authorAvatarPic;
+
 
     public MessageAdapter(Context context) {
         this.context = context;
+        currentUID = FirebaseAuth.getInstance().getUid();
+        mRef = FirebaseDatabase.getInstance().getReference();
+
     }
 
     public void add(Message message) {
+        if(!message.getAuthorID().equals(currentUID)) {
+            Log.d(TAG, "inside add, authorid =" + message.getAuthorID());
+            otherProfileUID = message.getAuthorID();
+            getProfilePic();
+        }
         this.messages.add(message);
         Collections.sort(this.messages, new Comparator<Message>() {
             @Override
@@ -47,6 +81,46 @@ public class MessageAdapter extends BaseAdapter {
             }
         });
         notifyDataSetChanged(); // to render the list we need to notify
+    }
+
+    public void getProfilePic() {
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //retrieve user information from the database
+                try {
+                    mFirebaseMethods = new FirebaseMethods(context);
+                    UserSettings userSettings= mFirebaseMethods.getUserAccountSettings(dataSnapshot, otherProfileUID);
+                    otherProfilePicURL = userSettings.getSettings().getProfile_photo();
+                    if(otherProfilePicURL != null && otherProfilePicURL.length() > 1) {
+                        Glide.with(context)
+                                .load(otherProfilePicURL)
+                                .into(authorAvatar);
+                    } else {
+                        authorAvatar.setImageResource(R.drawable.profile_default_pic);
+                    }
+                    Log.d(TAG, "Setting otherprofilepic to" + otherProfilePicURL);
+                } catch(Exception e) {
+                    Log.d(TAG, "could not get other profile pic");
+                    e.printStackTrace();
+                }
+                //retrieve images for the user in question
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+            private ValueEventListener init(ImageView avatar) {
+                authorAvatar = avatar;
+                return this;
+            }
+
+            private ImageView authorAvatar;
+        }.init(authorAvatarPic));
     }
 
     @Override
@@ -69,7 +143,7 @@ public class MessageAdapter extends BaseAdapter {
     public View getView(int i, View convertView, ViewGroup viewGroup) {
         MessageViewHolder holder = new MessageViewHolder();
         LayoutInflater messageInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        Message message = messages.get(i);
+        final Message message = messages.get(i);
 
         if (message.isBelongsToCurrentUser()) { // this message was sent by us so let's create a basic chat bubble on the right
             Log.d("Hello", "this is in the getView method");
@@ -78,27 +152,47 @@ public class MessageAdapter extends BaseAdapter {
             convertView.setTag(holder);
             holder.messageBody.setText(message.getText());
         } else { // this message was sent by someone else so let's create an advanced chat bubble on the left
-            Log.d("Hello", "this is in the getView method");
+            Log.d("Hello", "this is in the getView method, other guys");
             convertView = messageInflater.inflate(R.layout.their_message, null);
-            holder.avatar = (View) convertView.findViewById(R.id.image_message_profile);
+            holder.avatar = (ImageView) convertView.findViewById(R.id.image_message_profile);
             holder.name = (TextView) convertView.findViewById(R.id.text_message_name);
             holder.messageBody = (TextView) convertView.findViewById(R.id.text_message_body);
             holder.messageDate = (TextView) convertView.findViewById(R.id.text_message_time);
+            otherProfileUID = message.authorID;
+            if(otherProfileUID != null) {
+                Log.d(TAG, "trying to get other profile pic, authorID is " + otherProfileUID);
+                authorAvatarPic = holder.avatar;
+                getProfilePic();
+            }
             convertView.setTag(holder);
 
             //holder.name.setText(message.getData().getName());
             holder.name.setText(message.getAuthor());
             holder.messageBody.setText(message.getText());
             holder.messageDate.setText(message.date);
+            Log.d("MsgAdpter",  "authorid is " + message.getAuthorID());
+            holder.avatar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("MsgAdpterProfileClick",  "authorid is " + message.getAuthorID());
+                    Log.d(TAG, "The author pic icon was clicked");
+                    Intent intent1 = new Intent(context, ProfilePreviewActivity.class);
+                    intent1.putExtra("authorUID", message.getAuthorID());
+                    intent1.putExtra("ACTIVITY_NUM", 4); // so it will highlight bottom nav as itemlisting
+                    context.startActivity(intent1);
+                }
+            });
+            /*
             GradientDrawable drawable = (GradientDrawable) holder.avatar.getBackground();
             drawable.setColor(Color.parseColor("#FFFF00"));
+            */
         }
         return convertView;
     }
 }
 
 class MessageViewHolder {
-    public View avatar;
+    public ImageView avatar;
     public TextView name;
     public TextView messageBody;
     public TextView messageDate;
