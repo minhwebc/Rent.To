a3b1da255b6ad0b666067de208ef730e0a1da9a2
Brandon Chen <chenb777@uw.edu>
2018-05-05 11:40:08 -0700

    ask for permission on launch splash screen

--- app/src/main/java/to/rent/rentto/Home/WelcomeActivity.java
+++ app/src/main/java/to/rent/rentto/Home/WelcomeActivity.java
@@ -1,16 +1,45 @@
 package to.rent.rentto.Home;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import to.rent.rentto.R;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private final String TAG = "WelcomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Log.d(TAG, "Inside of onCreate");
+//        Intent intent = new Intent(this, HomeActivity.class);
+//        startActivity(intent);
+//        finish();
+        // Gets Permissions
+        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG,"Trying to get permission");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            start();
+        }
+    }
+
+    private void start() {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        start();
     }
 }
