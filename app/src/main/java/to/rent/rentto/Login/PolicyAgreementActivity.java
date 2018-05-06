ab361470399f6a818eb7b6073de0a2d076e5508e
Brandon Chen <chenb777@uw.edu>
2018-05-05 18:21:01 -0700

    add confirmation dialog to agree and decline

--- app/src/main/java/to/rent/rentto/Login/PolicyAgreementActivity.java
+++ app/src/main/java/to/rent/rentto/Login/PolicyAgreementActivity.java
@@ -1,4 +1,6 @@
 package to.rent.rentto.Login;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -44,14 +46,48 @@ public class PolicyAgreementActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 setResult(RESULT_OK);
-                finish();
+                AlertDialog.Builder builder = new AlertDialog.Builder(PolicyAgreementActivity.this);
+                builder.setMessage("I have read and agreed to Rent.to terms and conditions")
+                        .setTitle("Terms and Conditions")
+                        .setCancelable(true)
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setPositiveButton("Agree", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                finish();
+                            }
+                        });
+
+
+                AlertDialog alert = builder.create();
+                alert.show();
             }
         });
         decline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setResult(RESULT_CANCELED);
-                finish();
+                AlertDialog.Builder builder = new AlertDialog.Builder(PolicyAgreementActivity.this);
+                builder.setMessage("You need to agree to the terms and conditions to use Rent.to. \nAre you sure you want to decline?")
+                        .setTitle("Important")
+                        .setCancelable(true)
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                        })
+                        .setPositiveButton("Yes, I am sure", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                finish();
+                            }
+                        });
+
+
+                AlertDialog alert = builder.create();
+                alert.show();
             }
         });
     }
