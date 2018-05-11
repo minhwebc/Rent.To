4ac1ccdd0f5f353ed9f457d37cd1829a4e17a475
Quan <minhquan611996@gmail.com>
2018-05-11 16:05:01 -0700

    remove transition animation

--- app/src/main/java/to/rent/rentto/Utils/BottomNavigationViewHelper.java
+++ app/src/main/java/to/rent/rentto/Utils/BottomNavigationViewHelper.java
@@ -1,5 +1,6 @@
 package to.rent.rentto.Utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -51,28 +52,38 @@ public class BottomNavigationViewHelper {
                         Intent intent1 = new Intent(context, ItemsListActivity.class); //ACTIVITY_NUM = 0
                         intent1.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         context.startActivity(intent1);
+                        Activity something1 = (Activity) context;
+                        something1.overridePendingTransition(0,0); //0 for no animation
                         break;
                     case R.id.remind:
                         Intent intent3 = new Intent(context, RemindActivity.class); //ACTIVITY_NUM = 1
                         intent3.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         context.startActivity(intent3);
+                        Activity something2 = (Activity) context;
+                        something2.overridePendingTransition(0,0); //0 for no animation
                         break;
                     case R.id.ic_addImage:
                         Intent intent4 = new Intent(context, CameraActivity.class); //ACTIVITY_NUM = 1
                         intent4.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         context.startActivity(intent4);
+                        Activity something3 = (Activity) context;
+                        something3.overridePendingTransition(0,0); //0 for no animation
                         break;
 
                     case R.id.ic_profile:
                         Intent intent5 = new Intent(context, ProfileActivity.class); //ACTIVITY_NUM = 2
                         intent5.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         context.startActivity(intent5);
+                        Activity something4 = (Activity) context;
+                        something4.overridePendingTransition(0,0); //0 for no animation
                         break;
 
                     case R.id.ic_messages:
                         Intent intent6 = new Intent(context, NotificationActivity.class); // Activity_NUM = 3
                         intent6.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         context.startActivity(intent6);
+                        Activity something5 = (Activity) context;
+                        something5.overridePendingTransition(0,0); //0 for no animation
                         break;
                 }
                 return false;
