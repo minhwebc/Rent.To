9e89c9d3437cb05895cc07ac659a5f04dd376ad4
Brandon Chen <chenb777@uw.edu>
2018-05-17 16:23:46 -0700

    remove change email and phone number from editprofilefragment

--- app/src/main/java/to/rent/rentto/Profile/EditProfileFragment.java
+++ app/src/main/java/to/rent/rentto/Profile/EditProfileFragment.java
@@ -44,73 +44,10 @@ import static android.app.Activity.RESULT_OK;
  * Created by allencho on 2/15/18.
  */
 
-public class EditProfileFragment extends Fragment implements
-    ConfirmPasswordDialog.OnConfirmPasswordListener{
+public class EditProfileFragment extends Fragment {
     private static final String TAG = "EditProfileFragment";
     private static final int CHANGE_PROFILE_PIC = 1;
 
-    @Override
-    public void onConfirmPassword(String password) {
-        Log.d(TAG, "onConfirmPassword: got the password: " + password);
-
-        // Get auth credentials from the user for re-authentication. The example below shows
-        // email and password credentials but there are multiple possible providers,
-        // such as GoogleAuthProvider or FacebookAuthProvider.
-        AuthCredential credential = EmailAuthProvider
-                .getCredential(mAuth.getCurrentUser().getEmail(), password);
-
-        ///////////////////// Prompt the user to re-provide their sign-in credentials
-        mAuth.getCurrentUser().reauthenticate(credential)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            Log.d(TAG, "User re-authenticated.");
-
-                            ///////////////////////check to see if the email is not already present in the database
-                            mAuth.fetchProvidersForEmail(mEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<ProviderQueryResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<ProviderQueryResult> task) {
-                                    if(task.isSuccessful()){
-                                        try{
-                                            if(task.getResult().getProviders().size() == 1){
-                                                Log.d(TAG, "onComplete: that email is already in use.");
-                                                Toast.makeText(getActivity(), "That email is already in use", Toast.LENGTH_SHORT).show();
-                                            }
-                                            else{
-                                                Log.d(TAG, "onComplete: That email is available.");
-
-                                                //////////////////////the email is available so update it
-                                                mAuth.getCurrentUser().updateEmail(mEmail.getText().toString())
-                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                            @Override
-                                                            public void onComplete(@NonNull Task<Void> task) {
-                                                                if (task.isSuccessful()) {
-                                                                    Log.d(TAG, "User email address updated.");
-                                                                    Toast.makeText(getActivity(), "Email updated", Toast.LENGTH_SHORT).show();
-                                                                    mFirebaseMethods.updateEmail(mEmail.getText().toString());
-                                                                }
-                                                            }
-                                                        });
-                                            }
-                                        }catch (NullPointerException e){
-                                            Log.e(TAG, "onComplete: NullPointerException: "  +e.getMessage() );
-                                        }
-                                    }
-                                }
-                            });
-
-
-
-
-
-                        }else{
-                            Log.d(TAG, "onComplete: re-authentication failed.");
-                        }
-
-                    }
-                });
-    }
 
     //firebase
     private FirebaseAuth mAuth;
@@ -121,7 +58,7 @@ public class EditProfileFragment extends Fragment implements
     private String userID;
 
     //EditProfile Fragment widgets
-    private EditText mDisplayName, mUsername, mWebsite, mDescription, mEmail, mPhoneNumber;
+    private EditText mDisplayName, mUsername, mWebsite, mDescription;
     private TextView mChangeProfilePhoto;
     private CircleImageView mProfilePhoto;
 
@@ -141,8 +78,6 @@ public class EditProfileFragment extends Fragment implements
         mUsername = (EditText) view.findViewById(R.id.username);
         mWebsite = (EditText) view.findViewById(R.id.website);
         mDescription = (EditText) view.findViewById(R.id.description);
-        mEmail = (EditText) view.findViewById(R.id.email);
-        mPhoneNumber = (EditText) view.findViewById(R.id.phoneNumber);
         mChangeProfilePhoto = (TextView) view.findViewById(R.id.changeProfilePhoto);
         changeOtherSettingsButton = (Button) view.findViewById(R.id.buttonOtherSettings);
         mFirebaseMethods = new FirebaseMethods(getActivity());
@@ -221,68 +156,13 @@ public class EditProfileFragment extends Fragment implements
         final String username = mUsername.getText().toString();
         final String website = mWebsite.getText().toString();
         final String description = mDescription.getText().toString();
-        final String email = mEmail.getText().toString();
-
-        Long phoneNumber = new Long(0);
-        try {
-            phoneNumber = Long.parseLong(mPhoneNumber.getText().toString());
-        } catch(Exception e) {
-            Toast.makeText(getActivity(), "Please enter a valid phone number", Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        }
-        final Long checkPhone = phoneNumber;
-
-        Query query = myRef.child("users");
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                boolean exists = false;
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    User user = snapshot.getValue(User.class);
-                    if(user.getPhone_number().equals(Long.toString(checkPhone))){
-                        exists = true;
-                        break;
-                    }
-                }
-                if (exists) {
-                    Toast.makeText(getActivity(), "Phone number already exists", Toast.LENGTH_SHORT).show();
-                } else {
-                    String newPhone = Long.toString(checkPhone);
-                    System.out.println("My new phone number is " + newPhone);
-                    myRef.child("users").child(mAuth.getCurrentUser().getUid()).child("phone_number").setValue(newPhone);
-                    mFirebaseMethods.updateUserAccountSettings(null, null, null, checkPhone);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
         Log.d(TAG, "saveProfileSettings: displayName: "+ displayName);
         Log.d(TAG, "saveProfileSettings: website: " + website);
         Log.d(TAG, "saveProfileSettings: description " + description);
-        Log.d(TAG, "saveProfileSettings: phoneNumber: " + phoneNumber);
 
         //case1: if the user made a change to their username
         if(!mUserSettings.getUser().getUsername().equals(username)){
-
             checkIfUsernameExists(username);
-        }
-        //case2: if the user made a change to their email
-        if(!mUserSettings.getUser().getEmail().equals(email)){
-
-            // step1) Reauthenticate
-            //          -Confirm the password and email
-            ConfirmPasswordDialog dialog = new ConfirmPasswordDialog();
-            dialog.show(getFragmentManager(), getString(R.string.confirm_password_dialog));
-            dialog.setTargetFragment(EditProfileFragment.this, 1);
-
-
-            // step2) check if the email already is registered
-            //          -'fetchProvidersForEmail(String email)'
-            // step3) change the email
-            //          -submit the new email to the database and authentication
         }
 
         /**
@@ -291,8 +171,6 @@ public class EditProfileFragment extends Fragment implements
         String firebaseDisplayName = mUserSettings.getSettings().getDisplay_name();
         String firebaseWebsite = mUserSettings.getSettings().getWebsite();
         String firebaseDescription = mUserSettings.getSettings().getDescription();
-        String firebasePhoneNumber = mUserSettings.getUser().getPhone_number();
-
         if(firebaseDisplayName == null || !mUserSettings.getSettings().getDisplay_name().equals(displayName)){
             Log.d(TAG, "saveProfileSettings: displayName: "+ displayName);
             //update displayname
@@ -307,13 +185,6 @@ public class EditProfileFragment extends Fragment implements
             Log.d(TAG, "saveProfileSettings: description " + description);
             //update description
             mFirebaseMethods.updateUserAccountSettings(null, null, description, 0);
-        }
-        if(firebasePhoneNumber == null || !(mUserSettings.getUser().getPhone_number().equals(phoneNumber))) {
-
-                //.getUser().getPhone_number()().equals(phoneNumber)){
-            Log.d(TAG, "saveProfileSettings: phoneNumber: " + phoneNumber);
-            //update phoneNumber
-            mFirebaseMethods.updateUserAccountSettings(null, null, null, phoneNumber);
         }
     }
 
@@ -368,8 +239,6 @@ public class EditProfileFragment extends Fragment implements
         mUsername.setText(settings.getUsername());
         mWebsite.setText(settings.getWebsite());
         mDescription.setText(settings.getDescription());
-        mEmail.setText(userSettings.getUser().getEmail());
-        mPhoneNumber.setText(String.valueOf(userSettings.getUser().getPhone_number()));
     }
 
     /*
